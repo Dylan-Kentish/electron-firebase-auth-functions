6d68069d6e9d2aa9b4daa60659170117c56dcab2
Kent <dylankentish@live.co.uk>
2022-09-27 20:05:05 +0100

    corrected use of cors

--- functions/createAuthToken.js
+++ functions/createAuthToken.js
@@ -1,33 +1,28 @@
-const {initializeApp} = require("firebase-admin/app");
-const {getAuth} = require("firebase-admin/auth");
-const {getFirestore, doc, setDoc} = require("firebase/firestore");
-const cors = require("cors")({
-  origin: true,
-});
+const { initializeApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const { getFirestore, doc, setDoc } = require("firebase/firestore");
 
 initializeApp();
 
 exports.createAuthToken = (request, response) => {
-  cors(request, response, async () => {
-    const db = getFirestore();
-    const auth = getAuth();
+  const db = getFirestore();
+  const auth = getAuth();
 
-    const query = request.query;
-    const oneTimeCode = query["ot-auth-code"];
-    const idToken = query["id-token"];
+  const query = request.query;
+  const oneTimeCode = query["ot-auth-code"];
+  const idToken = query["id-token"];
 
-    const decodedToken = await auth.verifyIdToken(idToken);
+  const decodedToken = await auth.verifyIdToken(idToken);
 
-    const uid = decodedToken.uid;
+  const uid = decodedToken.uid;
 
-    const authToken = await auth.createCustomToken(uid);
+  const authToken = await auth.createCustomToken(uid);
 
-    console.log("Authentication token", authToken);
+  console.log("Authentication token", authToken);
 
-    await setDoc(doc(db, "ot-auth-codes", oneTimeCode), authToken);
+  await setDoc(doc(db, "ot-auth-codes", oneTimeCode), authToken);
 
-    return response.status(200).send({
-      token: authToken,
-    });
+  return response.status(200).send({
+    token: authToken,
   });
 };
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,23 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const {createAuthToken} = require("./createAuthToken");
+const cors = require("cors")({
+  origin: true,
+});
 
+// Set up a whitelist and check against it:
+const whitelist = ["http://electron-authentication.firebaseapp.com"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.get("/createAuthToken", createAuthToken);
 
 exports.api = functions.https.onRequest(app);
