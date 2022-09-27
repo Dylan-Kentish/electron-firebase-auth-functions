52d124131f9bb337f67b9b513fbf7fe85b3e5a4d
Kent <dylankentish@live.co.uk>
2022-09-27 20:50:40 +0100

    Improved logging

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`Access from "${origin}" not allowed by CORS`));
     }
   },
 };
