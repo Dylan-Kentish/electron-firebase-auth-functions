5a80cfa4020b8081fb8d174879efa2bfb5fdd0d8
Kent <dylankentish@live.co.uk>
2022-09-29 21:53:27 +0100

    the function doesnt require firestore, o the ot-code it just returns the custom auth token

--- functions/createAuthToken.js
+++ functions/createAuthToken.js
@@ -1,17 +1,13 @@
 const {initializeApp} = require("firebase-admin/app");
 const {getAuth} = require("firebase-admin/auth");
-const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
 
 exports.createAuthToken = (request, response) => {
   console.log("createAuthToken");
 
-  const db = getFirestore();
   const auth = getAuth();
-
   const query = request.query;
-  const oneTimeCode = query["ot-auth-code"];
   const idToken = query["id-token"];
 
   return auth.verifyIdToken(idToken)
@@ -20,12 +16,9 @@ exports.createAuthToken = (request, response) => {
 
         auth.createCustomToken(uid)
             .then((authToken) => {
-              db.doc(`ot-auth-codes/${oneTimeCode}`).set({authToken})
-                  .then(() => {
-                    return response.status(200).send({
-                      token: authToken,
-                    });
-                  });
+              return response.status(200).send({
+                token: authToken,
+              });
             });
       });
 };
